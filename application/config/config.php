37d584acabff15fd3397281993f2d4298acc7d84
pankzid <pankzid@gmail.com>
2014-09-03 13:09:06 +0700

    setting up base controller

--- application/config/config.php
+++ application/config/config.php
@@ -1,5 +1,15 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
+function __autoload($classname)
+{
+  if (strpos($classname, 'MY_') !==0) {
+    $file = APPPATH.'libraries/'.$classname.'.php';
+    if (file_exists($file) && is_file($file)) {
+      // var_dump($file);
+      @include_once $file;
+    }
+  }
+}
 /*
 |--------------------------------------------------------------------------
 | Base Site URL
---
+++ application/libraries/Admin_Controller.php
@@ -0,0 +1,12 @@
+<?php
+class Admin_Controller extends MY_Controller {
+
+  public function __construct()
+  {
+    parent::__construct();
+  }
+
+}
+
+/* End of file Admin_Controller.php */
+/* Location: ./application/libraries/Admin_Controller.php */
